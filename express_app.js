d42cc735aa86bf7e1733c53ee80469d6c5492e5c
kaleakash <akash300383@gmail.com>
2021-04-23 16:52:56 +0530

    done changes

--- express_app.js
+++ express_app.js
@@ -1,7 +1,7 @@
 let app = require("express")()
 
-app("/",(req,res)=> {
+app.get("/",(req,res)=> {
 	res.send("Successfull done the message with Jenkin")
 })
 
-app.listen(9999,()=>console.log("running server"))
+app.listen(9999,()=>console.log("Running on port number 9999"))
